2a1569a217121eff8402ef9a3ad33e7dad0e6502
= <gwethir@gmail.com>
2012-02-26 02:17:42 +0100

    Fixed post request instead of get

--- src/com/liongrid/icarus/Utils.java
+++ src/com/liongrid/icarus/Utils.java
@@ -4,45 +4,98 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.util.Log;
 
 public class Utils {
 
+	public static String executeHttpPost(String url, String action, String data){
+		// Create a new HttpClient and Post Header
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(url);
+		
+		BufferedReader in = null;
+		try {
+			// Add your data
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+			nameValuePairs.add(new BasicNameValuePair("action", action));
+			nameValuePairs.add(new BasicNameValuePair("data", data));
+			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+			// Execute HTTP Post Request
+			HttpResponse response = httpclient.execute(httppost);
+
+			in = new BufferedReader
+					(new InputStreamReader(response.getEntity().getContent()));
+			StringBuffer sb = new StringBuffer("");
+			String line = "";
+			String NL = System.getProperty("line.separator");
+			while ((line = in.readLine()) != null) {
+				sb.append(line + NL);
+			}
+			in.close();
+			String page = sb.toString();
+			Log.d("Icarus", "Server response: "+page);
+			return page;
+
+
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return "could not connect";
+	}
+
 	public static String executeHttpGet(String url) throws Exception {
 		Log.d("Icarus", "Executing http get request!  "+url);
-	    BufferedReader in = null;
-	    try {
-	        HttpClient client = new DefaultHttpClient();
-	        HttpGet request = new HttpGet();
-	        request.setURI(new URI(url));
-	        HttpResponse response = client.execute(request);
-	        in = new BufferedReader
-	        (new InputStreamReader(response.getEntity().getContent()));
-	        StringBuffer sb = new StringBuffer("");
-	        String line = "";
-	        String NL = System.getProperty("line.separator");
-	        while ((line = in.readLine()) != null) {
-	            sb.append(line + NL);
-	        }
-	        in.close();
-	        String page = sb.toString();
-	        Log.d("Icarus", "Server response: "+page);
-	        return page;
-	        } finally {
-	        if (in != null) {
-	            try {
-	                in.close();
-	                } catch (IOException e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }
+		BufferedReader in = null;
+		try {
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet();
+			request.setURI(new URI(url));
+			HttpResponse response = client.execute(request);
+			in = new BufferedReader
+					(new InputStreamReader(response.getEntity().getContent()));
+			StringBuffer sb = new StringBuffer("");
+			String line = "";
+			String NL = System.getProperty("line.separator");
+			while ((line = in.readLine()) != null) {
+				sb.append(line + NL);
+			}
+			in.close();
+			String page = sb.toString();
+			Log.d("Icarus", "Server response: "+page);
+			return page;
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
